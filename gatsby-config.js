1ee65ac55703147a34be36c69e41431e036170e0
Marty Thierry <marty@martylouis.com>
2020-03-24 18:37:02 -0500

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `emcoast-org`,
+        short_name: `emcoast`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#319795`,
+        theme_color: `#319795`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
