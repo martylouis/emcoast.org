0d379b1b193f9fbf55778021a0a8ee2f0777d3ca
Marty Thierry <marty@martylouis.com>
2020-04-04 19:25:04 -0500

    chore: rename files

---
+++ src/components/header.jsx
@@ -0,0 +1,31 @@
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import React from 'react';
+
+const Header = () => {
+  const { site } = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+
+        }
+      }
+    }
+  `);
+  return (
+    <header className="text-white bg-teal-600">
+      <div className="container">
+        <div className="items-center justify-between py-4 md:flex">
+          <Link to="/" className="text-lg font-bold leading-none">
+            {site.siteMetadata.title}
+            {' '}
+            <span className="font-light">Announcements</span>
+          </Link>
+          <div className="text-xs text-teal-200">Last Updated: April 4, 2020</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
