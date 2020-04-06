9f719397f040627d964fb2a1ca0f3408c1a99cae
Marty Thierry <marty@martylouis.com>
2020-04-06 11:32:11 -0500

    feat: Add public talk schedule

--- src/components/zoom.jsx
+++ src/components/zoom.jsx
@@ -3,20 +3,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ZoomMeeting = ({
-  day, time, downloadLink, downloadLabel,
+  day, time, downloadLink,
 }) => (
   <div className="w-56 p-6 mx-auto my-4 text-center bg-white border border-gray-300 rounded shadow-lg">
     <div className="text-xl font-medium leading-none text-teal-600 uppercase">{day}</div>
     <div className="mt-2 text-3xl font-extrabold leading-none tracking-tighter text-teal-800">{time}</div>
     {downloadLink
       ? (
-        <a className="flex items-center justify-center p-1 mt-4 text-sm font-bold text-white bg-teal-600 rounded hover:bg-teal-700" href={downloadLink} download>
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-1 -ml-1 fill-current" viewBox="0 0 24 24">
-            <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </svg>
-          {`Download ${downloadLabel}`}
-        </a>
+        <div>
+          <a href={downloadLink} download className="flex items-center justify-center p-1 mt-4 text-sm font-bold text-white bg-teal-600 rounded hover:bg-teal-700">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-1 -ml-1 fill-current" viewBox="0 0 24 24">
+              <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+            Download Schedule
+          </a>
+        </div>
       )
       : (
         <div disabled className="flex items-center justify-center p-1 mt-4 text-sm font-bold text-white bg-teal-600 rounded opacity-50 cursor-not-allowed">
@@ -31,6 +33,5 @@ export default ZoomMeeting;
 ZoomMeeting.propTypes = {
   day: PropTypes.string,
   time: PropTypes.string,
-  downloadLabel: PropTypes.string,
   downloadLink: PropTypes.string,
 };
