6b7fdb336ffa76d3360314057623f77d24824b4e
Marty Thierry <marty@martylouis.com>
2020-09-08 16:06:04 -0500

    Update download data

---
+++ src/data/downloads.js
@@ -0,0 +1,18 @@
+export const downloads = {
+  sunday: {
+    day: 'Sunday',
+    time: '10:00 AM',
+    fileName: 'Public_Talks',
+    updated: '2020-09-06',
+  },
+  tuesday: {
+    day: 'Tuesday',
+    time: '7:00 PM',
+    fileName: 'Midweek_Meeting',
+    updated: '2020-08-27',
+  },
+  fsg: {
+    fileName: 'Field_Service_Groups',
+    updated: '2020-06-20',
+  },
+};
