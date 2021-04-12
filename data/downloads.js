5103981aa216395a8e4aa92ce5429b9e83653420
Marty Thierry <marty@martylouis.com>
2021-04-12 10:31:06 -0500

    Update all docs
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- data/downloads.js
+++ data/downloads.js
@@ -1,25 +1,25 @@
 module.exports = {
-  all_updated: '2021-03-02T14:11:21',
+  all_updated: '2021-04-12T10:24:48',
   sunday: {
     meeting: 'Public Talk & Watchtower',
     day: 'Sunday',
     time: '10:00 AM',
     title: 'Public Talk Schedule',
-    fileName: 'Schedule_PT--2021-02-15',
-    updated: '2021-02-15T22:00:09',
+    fileName: 'Schedule_PT--2021-04-12',
+    updated: '2021-04-12T10:24:48',
   },
   tuesday: {
     meeting: 'Our Christian Life & Ministry',
     day: 'Tuesday',
     time: '7:00 PM',
-    fileName: 'Schedule_CLM--2021-02-21',
+    fileName: 'Schedule_CLM--2021-04-12',
     title: 'Midweek Meeting Schedule',
-    updated: '2021-03-02T14:11:21',
+    updated: '2021-04-12T10:24:48',
   },
   fsg: {
     title: 'Field Service Group List',
-    fileName: 'FSG--2020-12-22',
-    updated: '2020-12-22T10:16:35',
+    fileName: 'FSG--2020-04-12',
+    updated: '2021-04-12T10:24:48',
   },
   meeting_alert: {
     display: false,
