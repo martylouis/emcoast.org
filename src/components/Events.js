e8c399dbbc4c58f3d6b2e35684e243d34b440b63
Marty Thierry <marty@martylouis.com>
2022-03-23 13:38:25 -0500

    feat: add event
    Signed-off-by: Marty Thierry <marty@martylouis.com>

--- src/components/Events.js
+++ src/components/Events.js
@@ -6,9 +6,18 @@ export default function EventsCard() {
     <div className="grid sm:grid-cols-2 gap-x-6 gap-y-4">
       {events.list?.map(({ title, date, location }) => (
         <Card key={title}>
-          <span className="text-xl">{title}</span> <br />
+          <span
+            className="text-xl"
+            dangerouslySetInnerHTML={{ __html: title }}
+          />{' '}
+          <br />
           <span className="font-bold">{date}</span> <br />
-          <span className="text-sm">{location}</span>
+          <a href={location.url} target="_blank" rel="noreferrer">
+            <span
+              className="text-sm"
+              dangerouslySetInnerHTML={{ __html: location.title }}
+            />
+          </a>
         </Card>
       ))}
     </div>
